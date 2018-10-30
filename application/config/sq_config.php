2fcbe52d13bb8217afd4b6b54a02f384b85da0a1
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2018-10-30 22:06:37 +0530

    hi

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
 <?php 
 		$sq_base_url='http://localhost/onlinequiz/';
-		$sq_hostname='localhost';
-		$sq_dbname='quizmaster';
-		$sq_dbusername='root';
-		$sq_dbpassword='mysql';
+		$sq_hostname='omg.cmymhp6ktaco.us-east-2.rds.amazonaws.com';
+		$sq_dbname='onlinetest';
+		$sq_dbusername='omg_root';
+		$sq_dbpassword='1Human_race';
 		?>
