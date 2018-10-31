aec010526e4c16e5be1d6f5b26c0dba2a98c5602
kashwini24 <kashwini2404@gmail.com>
2018-11-01 02:56:39 +0530

    add remote database

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
 <?php
 		$sq_base_url='http://localhost/onlinequiz/';
-		$sq_hostname='localhost';
-		$sq_dbname='onlinequiz';
-		$sq_dbusername='root';
-		$sq_dbpassword='';
+		$sq_hostname='omg.cmymhp6ktaco.us-east-2.rds.amazonaws.com';
+		$sq_dbname='onlinetest';
+		$sq_dbusername='omg_root';
+		$sq_dbpassword='1Human_race';
 		?>
