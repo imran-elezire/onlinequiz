0e764f877269e9039584e112307cd03093f8863b
asmita <kashwini2404@gmail.com>
2019-01-17 17:15:45 +0530

    changes edit quiz or tcpdf

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -5,6 +5,8 @@
 		$sq_dbusername='elezire-admin';
 		$sq_dbpassword='elezire-password';
 
+		// $sq_base_url='http://localhost/onlinequiz/';
+
 		// $sq_hostname='localhost';
 		// $sq_dbname='onlinequiz';
 		// $sq_dbusername='root';
